1babd9c6fefb6aa5cd06eab43338030151952aa1
Ashley <codingbaee@gmail.com>
2024-02-09 12:03:56 -0500

    all done :) completed js for the alerts to display the dates/times with the corresponding city.

--- SheCodes_Plus_Add_onWeek 1 Homework_Timezone Dropdown/src/index.js
+++ SheCodes_Plus_Add_onWeek 1 Homework_Timezone Dropdown/src/index.js
@@ -1,1 +1,12 @@
+function showCityDate(event) {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM Do, YYYY hh:mm A");
 
+    let parisTime = alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+
+let citiesSelect = document.querySelector("#cities");
+citiesSelect.addEventListener("change", showCityDate);
